53d5c0d0eb2b2130157ac4f8fbfe0f926efd919a
kemalZaki <kemalmzaki@gmail.com>
2026-01-11 12:17:15 +0700

    add components

---
+++ src/components/Hero.tsx
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <section style={{ padding: "80px", textAlign: "center" }}>
+      <h1>Hi, saya Kemal 👋</h1>
+      <p>Web Developer | React | Cybersecurity Enthusiast</p>
+    </section>
+  );
+}
